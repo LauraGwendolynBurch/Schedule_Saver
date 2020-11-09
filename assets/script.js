0f8f3d66f2ee1811456d104f73a51e6f798846ac
Laura Gwen <lauraburch.lb@gmail.com>
2020-11-09 16:44:07 -0500

    added new html

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+// Get current hour of the day moment.js
+
+// for loop over the hours of the day from 9am to i <= 17
+
+//for loop get one place to another (aka start and stop)
+
+//       var i = 9; in html 
+//       element id add id= "#hour-" + i;
+//       Current checking 9am < current hour (also check less than and equal to)
+//       9am is in the past
+
+// Option a: .is-past, .is-present, .is-future
+//  select matching element $( element ID ).addClass("is-past")
+
+// option b: select matching element $( element ID ).css("background")
+
+// build matching html to css
+
+// add click event on a parent EL that can listen to my save buttons clicks
+
+// event.target
